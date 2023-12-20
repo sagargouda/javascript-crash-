38fc644e15a8350345238f378abf0d75b4f33a6a
sagar <sagarc2580@gmail.com>
2023-12-20 13:06:28 +0530

    js architecture

---
+++ jsEngine/index.js
@@ -0,0 +1,21 @@
+// slice method
+// let arr = [1, 2, 3, 4, 5];
+// let arrCopy = arr;
+// console.log(arr.slice(2, 3));
+// console.log(arrCopy.slice(1, 2));
+// console.log(arr);
+// console.log(arrCopy);
+
+// splice method
+// let arr = [1, 2, 3, 4, 5];
+// let arrCopy = arr;
+// console.log(arrCopy.splice(2, 2));
+// console.log(arr.splice(0, 4));
+// console.log(arr);
+// console.log(arrCopy);
+
+// for (let i = 1; i <= 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, i * 1000);
+// }
---
+++ jsEngine/notes.md
@@ -0,0 +1,8 @@
+1. to run js code you need js engine , web api etc etc ,so js runtime environment is like container , js engine is like heart of js runtime environment
+2. node js is runtime environment , which has evyrthing to run
+
+3) settimeout is pressent in both runtime ennvironemnt also node
+
+4) first js engine is spider monkkey used by mozila
+
+5)
